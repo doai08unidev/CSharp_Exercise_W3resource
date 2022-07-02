b2332edd26309f0b78bfa1c3eac1a51b0be52b29
doai08unidev <doai08.unidev@gmail.com>
2022-07-02 12:19:16 +0700

    no message

---
+++ Ex1.cs
@@ -0,0 +1,12 @@
+/*1. Write a C# Sharp program to print Hello and your name in a separate line.
+Expected Output :
+Hello: Alexandra Abramov*/
+using System;
+namespace CSharpExerciseW3resource{
+    public class Ex1{
+        public static void Main(string[] args){
+            System.Console.WriteLine("Hello");
+            System.Console.WriteLine("Nguyen Van Doai");
+        }
+    }
+}
